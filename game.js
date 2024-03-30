84a2d1d30379f38320fda91d50dcd07e4bd1a14f
SashaNikitich <sashanikitich@gmail.com>
2024-03-30 21:32:26 +0200

    update-background

--- game.js
+++ game.js
@@ -32,7 +32,7 @@ var endText;
 
 function preload() {
     // Load assets
-    this.load.image('background', 'assets/background/space.jpg');
+    this.load.image('background', 'assets/background/space.png');
     this.load.image('player', 'assets/player.png');
     this.load.image('object1', 'assets/spacemoor-1.png')
     this.load.image('object2', 'assets/spacemoor-2.png')
@@ -42,7 +42,7 @@ function create ()
 {
 
     //Add background
-    this.background = this.add.image(0, 0, "background").setScale(5,5);
+    this.background = this.add.image(500, 250, "background");
 
     //#region Player
     player = this.physics.add.sprite(100, 450, 'player').setScale(0.15)
